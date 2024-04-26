8ead6b004fe9c52f35a34b92d5087dce82cc74d5
Kumaraguru Tholasiraman <kumaraguru.tholasiraman@dxc.com>
2024-04-26 08:54:16 +0000

    my first stable version

---
+++ app/purchaseorderapp/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("guru.po.purchaseorderapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
